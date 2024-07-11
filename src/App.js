0966130858d4180a651d9f0395968b4ebc2c0603
VishnuXT <vishnu.nrd1234@gmail.com>
2024-07-11 14:48:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Adduser from './Components/1';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Search from './Components/3';
+import Displaydata from './Components/2';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+<Routes>
+<Route path='/' element={<Adduser/>}/>
+<Route path='/search' element={ <Search/>}/>
+<Route path='/display' element={<Displaydata/>}/>
+</Routes>
+    
+    </BrowserRouter>
   );
 }
 
